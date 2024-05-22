13de41d04851ddf2cee87f6c8256823804d13ba7
Riya Khandelwal <riyakhandelwal@jklu.edu.in>
2024-05-22 14:30:44 +0530

    modified:   Python_Assignment_1/Basic_Level/Q7.py

--- Python_Assignment_1/Basic_Level/Q7.py
+++ Python_Assignment_1/Basic_Level/Q7.py
@@ -1,23 +1,8 @@
+import math
 num1=int(input("Enter the num1 here: "))
 num2=int(input("Enter the second no here : "))
-list1=[]
-list2=[]
-for i in range (1,num1+1):
-    if (num1%i==0):
-        list1.append(i)
-    else :
-        continue
-for j in range (1,num2+1):
-    if (num2%j==0):
-        list2.append(j)
-    else :
-        continue
 
-lcm=1
-for i in list1:
-    if i in list2:
-        lcm=lcm*i
-    else :
-        continue
+a=math.gcd(num1, num2)
 
-print("LCM is ", lcm)
+lcm=(num1*num2)//a
+print(lcm)
